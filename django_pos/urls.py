8d7059bd2cbb571cf28941eec049083a68e92637
Parth Sharma <parth261297@gmail.com>
2018-08-11 18:07:18 +0530

    Basic app setup

--- django_pos/urls.py
+++ django_pos/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('pos/', include('pos.urls')),
 ]
