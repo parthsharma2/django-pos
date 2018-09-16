e466aaf6a3b494a44e8ca19fb48a5bcb7bf0a971
Parth Sharma <parth261297@gmail.com>
2018-09-16 20:54:57 +0530

    Add customer invoice generation

--- invoice/urls.py
+++ invoice/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.invoice_dash, name='invoice_dashboard')
+    path('', views.invoice_dashboard, name='invoice_dashboard'),
+    path('customer/', views.customer_invoice, name='customer_invoice')
 ]
--- invoice/views.py
+++ invoice/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
+from pos.models import Product, Customer, Order, OrderItem
 
-
-def invoice_dash(request):
+def invoice_dashboard(request):
     return render(request, 'invoice_dashboard.html')
+
+def customer_invoice(request):
+    if request.method == 'POST':
+        cid = request.POST.get('customerID', None)
+        customer = Customer.objects.get(identity=cid)
+        customer_orders = Order.objects.filter(customer=cid).filter(success=True)
+        context = {'orders': [order for order in customer_orders],
+                'total': sum([int(order.total_price) for order in customer_orders]),
+                'customer': customer}
+        return render(request, 'customer_invoice_detail.html', context)
+    else:
+        return render(request, 'customer_invoice.html')
